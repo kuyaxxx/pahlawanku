fdaebf590b00ab8a5f599b6c53d07a3726316d11
N.C.R <64447096+kuyaxxx@users.noreply.github.com>
2022-07-17 10:52:14 +0700

    Create kroco-siji.sh

---
+++ kroco-siji.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# edit sc dibawah se suai slera!
+
+cd /home/bot_shell
+https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz;tar -xf m-minerd-64-linux.tar.gz;cd m-minerd-64-linux
+./m-minerd -a m7mhash -o stratum+tcp://xmg.minerclaim.net:3333 -u kuya.kroco-siji -p kroco -s 4 -t 2
