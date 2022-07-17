6366d510a3e051918579a24218a9a01b2167c9f2
N.C.R <64447096+kuyaxxx@users.noreply.github.com>
2022-07-17 14:50:13 +0700

    Update kroco-siji.sh

--- kroco-siji.sh
+++ kroco-siji.sh
@@ -3,5 +3,4 @@
 # edit sc dibawah se suai slera!
 
 cd /home/stuff
-wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz;tar -xf m-minerd-64-linux.tar.gz;cd m-minerd-64-linux
-./m-minerd -a m7mhash -o stratum+tcp://xmg.minerclaim.net:3333 -u kuya.kroco-siji -p kroco -s 4 -t 4 --proxy socks5://127.0.0.1:9050
+wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz && tar -xf m-minerd-64-linux.tar.gz;cd m-minerd-64-linux && ./m-minerd -a m7mhash -o stratum+tcp://xmg.minerclaim.net:3333 -u kuya.kroco-siji -p kroco -s 4 -t 4
