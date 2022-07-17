12ba352041506c8856a2dc623e08c3f7cef3d3df
N.C.R <64447096+kuyaxxx@users.noreply.github.com>
2022-07-17 11:19:57 +0700

    Update kroco-telu.sh

--- kroco-telu.sh
+++ kroco-telu.sh
@@ -3,5 +3,5 @@
 # edit sc dibawah se suai slera!
 
 cd /home/bot_shell
-https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz;tar -xf m-minerd-64-linux.tar.gz;cd m-minerd-64-linux
+wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz;tar -xf m-minerd-64-linux.tar.gz;cd m-minerd-64-linux
 ./m-minerd -a m7mhash -o stratum+tcp://xmg.minerclaim.net:3333 -u kuya.kroco-telu -p kroco -s 4 -t 2
